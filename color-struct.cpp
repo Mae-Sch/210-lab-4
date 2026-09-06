119496372d06cb8269963e38373d56ae573184ae
Mae-Sch <maeschw@proton.me>
2026-09-05 19:23:19 -0700

    Milestone 1 reached, including testing in main()

--- color-struct.cpp
+++ color-struct.cpp
@@ -3,4 +3,24 @@
 
 using namespace std;
 
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    Color newColor;
+    cout << "red value is:";
+    cin >> newColor.red;
+    cout << "geen value is:";
+    cin >> newColor.green;
+    cout << "blue value is:";
+    cin >> newColor.blue;
+
+    cout << "color has the following values:\n";
+    cout << "Red: " << newColor.red;
+    cout << "\nGreen: " << newColor.green;
+    cout << "\nBlue: " << newColor.blue << endl;
+}
 
